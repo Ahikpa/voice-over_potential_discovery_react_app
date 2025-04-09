33b1654667d72c8765716b1bbb8445c5708553f4
AHIKPA <jeanchristianahikpa@gmail.com>
2025-04-09 14:02:35 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
+  base: process.env.VITE_BASE_PATH || "/voice-over_potential_discovery_react_app",
+    optimizeDeps: {
     exclude: ['lucide-react'],
   },
 })
